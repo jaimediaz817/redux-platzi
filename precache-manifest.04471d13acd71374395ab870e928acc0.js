97fb22ce5472d97be85c5afb87b5d31de85b056e
Jaime Diaz <jdsolutions817@gmail.com>
2019-07-22 21:00:07 -0500

    Updates

---
+++ precache-manifest.04471d13acd71374395ab870e928acc0.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff66f1524340517e43503a97604b6a4c",
+    "url": "/redux-platzi/index.html"
+  },
+  {
+    "revision": "ddd5696a40cb8f13376c",
+    "url": "/redux-platzi/static/css/2.2c11f90b.chunk.css"
+  },
+  {
+    "revision": "430ee94bc63a60aab7e3",
+    "url": "/redux-platzi/static/css/main.9094ba12.chunk.css"
+  },
+  {
+    "revision": "ddd5696a40cb8f13376c",
+    "url": "/redux-platzi/static/js/2.136d54ef.chunk.js"
+  },
+  {
+    "revision": "430ee94bc63a60aab7e3",
+    "url": "/redux-platzi/static/js/main.e500aa9d.chunk.js"
+  },
+  {
+    "revision": "b56a9e7365ca36400773",
+    "url": "/redux-platzi/static/js/runtime~main.7aa7ce29.js"
+  },
+  {
+    "revision": "03783c5172ee1ad128c576bf88fac168",
+    "url": "/redux-platzi/static/media/fa-brands-400.03783c51.eot"
+  },
+  {
+    "revision": "073c2f3ce60eaf69cc2767ef3d989078",
+    "url": "/redux-platzi/static/media/fa-brands-400.073c2f3c.svg"
+  },
+  {
+    "revision": "7559b3774a0625e8ca6c0160f8f6cfd8",
+    "url": "/redux-platzi/static/media/fa-brands-400.7559b377.woff2"
+  },
+  {
+    "revision": "ed2b8bf117160466ba6220a8f1da54a4",
+    "url": "/redux-platzi/static/media/fa-brands-400.ed2b8bf1.ttf"
+  },
+  {
+    "revision": "fe9d62e0d16a333a20e63c3e7595f82e",
+    "url": "/redux-platzi/static/media/fa-brands-400.fe9d62e0.woff"
+  },
+  {
+    "revision": "59215032a4397507b80e5625dc323de3",
+    "url": "/redux-platzi/static/media/fa-regular-400.59215032.ttf"
+  },
+  {
+    "revision": "8fdea4e89ac405d9f8db327adb331d8d",
+    "url": "/redux-platzi/static/media/fa-regular-400.8fdea4e8.svg"
+  },
+  {
+    "revision": "e07d9e40b26048d9abe2ef966cd6e263",
+    "url": "/redux-platzi/static/media/fa-regular-400.e07d9e40.woff2"
+  },
+  {
+    "revision": "e5770f9863963fb576942e25214a226d",
+    "url": "/redux-platzi/static/media/fa-regular-400.e5770f98.woff"
+  },
+  {
+    "revision": "fc9c63c8224fb341fc933641cbdd12ef",
+    "url": "/redux-platzi/static/media/fa-regular-400.fc9c63c8.eot"
+  },
+  {
+    "revision": "4bced7c4c0d61d4f988629bb8ae80b8b",
+    "url": "/redux-platzi/static/media/fa-solid-900.4bced7c4.woff"
+  },
+  {
+    "revision": "acf50f59802f20d8b45220eaae532a1c",
+    "url": "/redux-platzi/static/media/fa-solid-900.acf50f59.ttf"
+  },
+  {
+    "revision": "b557f56e367e59344ca95f9d1fb44352",
+    "url": "/redux-platzi/static/media/fa-solid-900.b557f56e.svg"
+  },
+  {
+    "revision": "b5cf8ae26748570d8fb95a47f46b69e1",
+    "url": "/redux-platzi/static/media/fa-solid-900.b5cf8ae2.woff2"
+  },
+  {
+    "revision": "ef3df98419d143d9617fe163bf4edc0b",
+    "url": "/redux-platzi/static/media/fa-solid-900.ef3df984.eot"
+  }
+]);
